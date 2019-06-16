53035cf58ddb601eceb6a069078c0d06b2f32c0e
Vitaliy <makaruba@mail.ru>
2019-06-16 14:39:11 +0700

    HomeWork

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,23 +1,24 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
-    public static void main(String[] args){
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("Debug Message Logged !!!");
+        logger.info("Info Message Logged !!!");
+        logger.error("Error Message Logged !!!", new NullPointerException("NullError"));
+
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://otus.ru");
 
-        driver.quit();
-
-
-
-
-
-
     }
-
 }
